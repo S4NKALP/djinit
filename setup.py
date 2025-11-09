a8ce63d285726d9e03dbc93f365540cf3f04fecd
S4NKALP <sankalptharu50028@gmail.com>
2025-11-09 15:23:43 +0545

    chore: bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="djinitx",
-    version="0.1.0",
+    version="0.2.0",
     description="A CLI tool to setup Django projects",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
